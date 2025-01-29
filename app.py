50d165ff912aabf376260356af46d89fdbaad4cb
Martin Agoha <martin4dtruth@gmail.com.>
2025-01-29 04:51:01 +0100

    Fix: Updated datetime format to remove microseconds and add 'Z' for UTC

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ from datetime import datetime
 import pytz
 
 app = Flask(__name__)
-CORS(app)  
+CORS(app)
 
 @app.route('/', methods=['GET'])
-def get_infomation():
+def get_information():
+    # Get the current UTC time
+    current_datetime = datetime.now(pytz.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+    
     return jsonify({
         "email": "martinagoha7@gmail.com",
-        "current_datetime": datetime.now(pytz.utc).isoformat(),
+        "current_datetime": current_datetime,
         "github_url": "https://github.com/Martin4dbest/HNG12-Backend-stage0-api"
     }), 200
 
