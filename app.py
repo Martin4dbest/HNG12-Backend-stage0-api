72e3208ba47d05d93603e201ad219f53fac3af31
Martin Agoha <martin4dtruth@gmail.com.>
2025-01-29 03:39:36 +0100

    Added Flask API with email and GitHub URL response

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from datetime import datetime
+import pytz
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS
+
+@app.route('/', methods=['GET'])
+def get_infomation():
+    return jsonify({
+        "email": "martinagoha7@gmail.com",
+        "current_datetime": datetime.now(pytz.utc).isoformat(),
+        "github_url": "https://github.com/Martin4dbest/HNG12-Backend-stage0-api"
+    }), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
